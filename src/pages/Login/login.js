320ae1c35ca5e7e36657fa006226f79a7ad7e332
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-08-22 19:35:01 -0700

    Updating login.js to change to a class

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
 import "./login.css";
 
-function Login() {
-  return (
-    <React.Fragment>
+// function Login() {
+//   return (
+//     <React.Fragment>
+//       <div className="App">
+//         <header className="main-header">
+//           <img src="../transparent-bkg.png" className="logo" alt="logo" />
+//         </header>
+//       </div>
+
+//       <div className="bkg">
+//         <img
+//           src="../login-bkg-photo.jpg"
+//           className="bkg-photo"
+//           alt="background"
+//         />
+
+//         <div className="login-form">
+//           <div class="col">
+//             <form>
+//               <div class="form-group">
+//                 <input
+//                   type="text"
+//                   class="form-control"
+//                   placeholder="username"
+//                 />
+//               </div>
+//               <div class="form-group">
+//                 <input
+//                   type="password"
+//                   class="form-control"
+//                   placeholder="password"
+//                 />
+//               </div>
+//             </form>
+
+//             <button className="login-btn">
+//               <a href="/profile">Login</a>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+class Login extends Component {
+  render(){
+    return(
+      <React.Fragment>
       <div className="App">
         <header className="main-header">
           <img src="../transparent-bkg.png" className="logo" alt="logo" />
@@ -43,7 +89,8 @@ function Login() {
         </div>
       </div>
     </React.Fragment>
-  );
+    )
+  }
 }
 
 export default Login;

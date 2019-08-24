407c416f54ece07898a3f51009d5ccd87ff19255
loannpham87 <loannpham87@gmail.com>
2019-08-24 11:37:26 -0700

    added npms to package.json, edited gitignore file to include package-lock.json

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -10,19 +10,34 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+
+let mongoose = require("mongoose");
+let express = require("express");
+let app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/", {
+  useNewUrlParser: true
+});
+
+// const isLocalhost = Boolean(
+//   window.location.hostname === 'localhost' ||
+//     // [::1] is the IPv6 localhost address.
+//     window.location.hostname === '[::1]' ||
+//     // 127.0.0.1/8 is considered localhost for IPv4.
+//     window.location.hostname.match(
+//       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+//     )
+// );
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
+    app.use(express.static("src/build"));
+    app.get("*", () => () => {
+      res.sendFile(path.joim(__dirname, "src", "build", "index.html")); //relative path
+    })
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin

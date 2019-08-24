be0e0b6b70310ab241f4d1e6a15af98303b1492c
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-08-24 11:49:49 -0700

    Fixing issues with home page

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ const client = new ApolloClient({
          encrypted: true
         });
       }
+
       render() {
         return (
         <Router>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,37 +1,72 @@
 import React, { Component } from "react";
 import "./home.css";
 
-function Home() {
-  return (
-    <React.Fragment>
-      <div className="App">
-        <header className="main-header">
-          <img src="./../../images/transparent-bkg.png" className="logo" alt="logo" />
-          transparent-bkg.png
-        </header>
-      </div>
+// function Home() {
+//   return (
+//     <React.Fragment>
+//       <div className="App">
+//         <header className="main-header">
+//           <img src="./../../images/transparent-bkg.png" className="logo" alt="logo" />
+//           transparent-bkg.png
+//         </header>
+//       </div>
 
-      <div className="bkg">
-        <img
-          src="./../../images/initial-login-bkg-photo.jpg"
-          className="bkg-photo"
-          alt="background photo"
-        />
-      </div>
+//       <div className="bkg">
+//         <img
+//           src="./../../images/initial-login-bkg-photo.jpg"
+//           className="bkg-photo"
+//           alt="background"
+//         />
+//       </div>
 
-      <div className="btn-container">
-        <button className="btn">
-          <a href="/login">Login</a>
-        </button>
-        <br />
+//       <div className="btn-container">
+//         <button className="btn">
+//           <a href="/login">Login</a>
+//         </button>
+//         <br />
 
-        <button className="btn">
-          <a href="/signup">Signup</a>
-        </button>
-      </div>
+//         <button className="btn">
+//           <a href="/signup">Signup</a>
+//         </button>
+//       </div>
 
-    </React.Fragment>
-  );
+//     </React.Fragment>
+//   );
+// }
+
+class Home extends Component{
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <header className="main-header">
+            <img src="./../../images/transparent-bkg.png" className="logo" alt="logo" />
+            transparent-bkg.png
+          </header>
+        </div>
+  
+        <div className="bkg">
+          <img
+            src="./../../images/initial-login-bkg-photo.jpg"
+            className="bkg-photo"
+            alt="background"
+          />
+        </div>
+  
+        <div className="btn-container">
+          <button className="btn">
+            <a href="/login">Login</a>
+          </button>
+          <br />
+  
+          <button className="btn">
+            <a href="/signup">Signup</a>
+          </button>
+        </div>
+  
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;

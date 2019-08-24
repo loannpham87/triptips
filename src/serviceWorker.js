b112544241dec74f3df5c3c93269e2adb312e5e6
loannpham87 <loannpham87@gmail.com>
2019-08-24 13:33:05 -0700

    defined isLocalHost

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -30,15 +30,15 @@
 //   useNewUrlParser: true
 // });
 
-// const isLocalhost = Boolean(
-//   window.location.hostname === 'localhost' ||
-//     // [::1] is the IPv6 localhost address.
-//     window.location.hostname === '[::1]' ||
-//     // 127.0.0.1/8 is considered localhost for IPv4.
-//     window.location.hostname.match(
-//       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-//     )
-// );
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
 
 export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {

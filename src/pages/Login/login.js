9d52c18d9b91ea646fc2181b74d40d208c8c0004
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-08-26 15:20:27 -0700

    Rebuilt login page with new components

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,51 +1,9 @@
 import React, { Component } from "react";
-import Loginform from "./../../components/loginForm/loginForm";
+// import Loginform from "./../../components/loginForm/loginForm";
+import Form from "./../../components/form/form";
+import Input from "./../../components/input/input";
+import Button from "./../../components/button/button";
 import "./login.css";
-
-// function Login() {
-//   return (
-//     <React.Fragment>
-//       <div className="App">
-//         <header className="main-header">
-//           <img src="../transparent-bkg.png" className="logo" alt="logo" />
-//         </header>
-//       </div>
-
-//       <div className="bkg">
-//         <img
-//           src="../login-bkg-photo.jpg"
-//           className="bkg-photo"
-//           alt="background"
-//         />
-
-//         <div className="login-form">
-//           <div class="col">
-//             <form>
-//               <div class="form-group">
-//                 <input
-//                   type="text"
-//                   class="form-control"
-//                   placeholder="username"
-//                 />
-//               </div>
-//               <div class="form-group">
-//                 <input
-//                   type="password"
-//                   class="form-control"
-//                   placeholder="password"
-//                 />
-//               </div>
-//             </form>
-
-//             <button className="login-btn">
-//               <a href="/profile">Login</a>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </React.Fragment>
-//   );
-// }
 
 class Login extends Component {
   state = {
@@ -69,11 +27,11 @@ class Login extends Component {
   render(){
     return(
       <React.Fragment>
-      <div className="App">
+      {/* <div className="App">
         <header className="main-header">
           <img src="./../../images/transparent-bkg.png" className="logo" alt="logo" />
         </header>
-      </div>
+      </div> */}
 
       <div className="bkg">
         {/* <img
@@ -84,7 +42,11 @@ class Login extends Component {
 
         <div className="login-form">
           <div className="col">
-            <Loginform />
+            <Form>
+              <Input type="text" className="form-control" name="username" placeholder="username" onChange={this.handleChange}/>
+              <Input type="password" className="form-control" name="password" placeholder="password" onChange={this.handleChange}/>
+            </Form>
+            <Button className="login-btn" onClick={this.handleSubmit} text="Login"/>
           </div>
         </div>
       </div>

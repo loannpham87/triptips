8905ee1cfdcfe682943111ad6c70ff80336f3635
Loann Pham <48141082+loannpham87@users.noreply.github.com>
2019-08-30 00:21:28 -0700

    Update navbar.js

--- client/src/components/NavBar/navbar.js
+++ client/src/components/NavBar/navbar.js
@@ -7,6 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
+           tripstips
       </Link>
       <div>
         <ul className="navbar-nav">

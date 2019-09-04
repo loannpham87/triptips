f804e22bb5a7fe400b052b71509b5b5187db4c33
AmandaJ <amanda98528@gmail.com>
2019-09-03 21:10:05 -0700

    adding more Post stuff

---
+++ client/src/actions/photo.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import {
+    UPLOAD_SUCCESS,
+    FETCH_PHOTOS,
+    DELETE_PHOTO
+} from './types';
+
+export const uploadPhoto = (formData, userId) => async dispatch => {
+    try {
+        const upload = await axios.post(`/api/user/uploadImage/`, formData,
+        { headers: { Authorization: localStorage.getItem('token') } });
+        console.log(upload);
+        dispatch({ type: UPLOAD_SUCCESS, payload: upload });
+    } catch(err){
+        console.log(err);
+    }
+}
+
+export const fetchPhotos = () => async dispatch => {
+    try {
+        const photos = await axios.get(`/api/user/uploads`,
+        { headers: { Authorization: localStorage.getItem('token') } });
+        dispatch({ type: FETCH_PHOTOS, payload: photos });
+    } catch(err){
+        console.log(err);
+    }
+}
+
+export const deletePhoto = (imageId) => async dispatch => {
+    try {
+        const deleteImage = await axios.delete(`/api/user/deleteImage/${imageId}`,
+        { headers: { Authorization: localStorage.getItem('token') } });
+        dispatch({ type: DELETE_PHOTO, payload: deleteImage });
+    } catch(err){
+        console.log(err);
+    }
+}
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,12 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+
+
+/*
+PHOTO ACTIONS
+*/
+export const UPLOAD_SUCCESS = 'UPLOAD_SUCCESS';
+export const FETCH_PHOTOS = 'FETCH_PHOTOS';
+export const DELETE_PHOTO = 'DELETE_PHOTO';

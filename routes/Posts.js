90f767fe70c6c52f4f6e6e907d392ef382f998d5
Robert <robert.d.weston@gmail.com>
2019-09-06 19:16:40 -0700

    Fixed routes posts file

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,90 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
 
-const User = mongoose.model('users');
-const Post = mongoose.model('Post');
+//Load Post model
+const Post = require("./../models/Post");
 
-const keys = require('../config/keys');
+router.post("/upload", (req, res) => {
+    const newPost = new Post ({
+        userId: req.body.userId,
+        title: req.body.title,
+        userName: req.body.userName,
+        picUrl: req.body.picUrl,
+        caption: req.body.caption,
+    });
 
-const cloudinary = require('cloudinary');
-const multer = require('multer');
-
-const passport = require('passport');
-const requireAuth = passport.authenticate('jwt', { session: false });
-
-const upload = multer();
-
-cloudinary.config({
-    cloud_name: keys.cloudinaryName,
-    api_key: keys.cloudinaryAPIKey,
-    api_secret: keys.cloudinaryAPISecret
-})
-
-router.get('/uploads', requireAuth, async (req, res, next) => {
-    try {
-        const posts = await Post.find({ userId: req.user._id });
-        res.json(posts);
-    } catch(err){
-        console.log(err);
-    }
+    newPost
+        .save()
+        .then(post => res.json(post))
+        .catch(err => console.log(err));
 });
 
-// router.post('/uploadImage/', requireAuth, upload.single('photo'), (req, res, next) => {
-router.post('/uploadImage/', upload.single('photo'), (req, res, next) => {
-
-    cloudinary.v2.uploader.upload_stream({ resource_type: 'raw' }, async (error, result) => {
-        if(error){
-            console.log(err);
-        }
-        
-        const newPost = new Post({
-            title: 'Title',
-            caption: 'Caption',
-            imageUrl: result.url,
-            image_id: result.public_id,
-            profilePic: 'https://www.vccircle.com/wp-content/uploads/2017/03/default-profile.png' ,
-            userId: req.user._id
-        })
-
-        try {
-            const savePost = await newPost.save();
-            res.send(savePost);
-        } catch(err){
-            console.log(err);
-        }
-    }).end(req.file.buffer);
+router.get("/uploads", (req, res) => {
+    Post.find()
+        .then(posts => {
+            console.log(posts);
+            return res.json(posts);
+        }).catch(err => console.log(err));
 });
 
-// router.post('/uploadAvatar', requireAuth, (req, res) => {
-//     cloudinary.v2.uploader.upload_stream({ resource_type: 'raw' }, async (error, result) => {
-//         if(error){
-//             console.log(err);
-//         }
-        
-//         try {
-//             const user = User.findByIdAndUpdate(req.user._id, { $set: { profilePic: result.url }})
-//             res.send(user);
-//         } catch(err){
-//             res.send(500, { error: err });
-//         }
-//     }).end(req.file.buffer);
-// })
-
-// router.delete('/deleteImage/:imageId', requireAuth, (req, res) => {
-router.delete('/deleteImage/:imageId', (req, res) => {
-
-    const { imageId } = req.params;
-    console.log(imageId);
-
-    cloudinary.v2.uploader.destroy(imageId, { invalidate: true, resource_type: 'raw' }, async (error, result) => {
-        try {
-            const deleteImage = await Post.findOneAndRemove({ image_id: imageId, userId: req.user._id });
-            res.send(deleteImage);
-        } catch(err){
-            console.log(err);
-        }
-    })
+router.get("/uploads/:userId", (req, res) => {
+    console.log(req.params.userId);
+    Post.find({ userId: req.params.userId })
+        .then(posts => {
+            console.log(posts);
+            return res.json(posts);
+        }).catch(err => console.log(err));
 })
 
 module.exports = router;

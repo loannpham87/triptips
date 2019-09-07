505c7ab1a8fa4bb39f6987f49334401e89cc4b14
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-09-07 12:13:13 -0700

    Heroku deployment

--- server.js
+++ server.js
@@ -36,4 +36,12 @@ app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+    resonse.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));

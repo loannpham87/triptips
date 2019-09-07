55732e1aeb70e573796c1d8c1c5df0f78542125d
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-09-07 10:52:22 -0700

    Working build push

---
+++ client/build/precache-manifest.8f9b6ae7ebc0a2f677b695a104e6fbae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503608890d2cffa013ad8dd1da286cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71fbd6dee69fb81602d7",
+    "url": "/static/css/main.6e3110c9.chunk.css"
+  },
+  {
+    "revision": "5abaee1f56f011ecf7b2",
+    "url": "/static/js/2.87effc85.chunk.js"
+  },
+  {
+    "revision": "71fbd6dee69fb81602d7",
+    "url": "/static/js/main.d51982f2.chunk.js"
+  },
+  {
+    "revision": "e78496358e58085f0dde",
+    "url": "/static/js/runtime~main.9eecdd64.js"
+  }
+]);

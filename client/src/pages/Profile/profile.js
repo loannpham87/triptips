19d78496e3bd6dc2e4cc34c569cc16d4f274017c
Robert <robert.d.weston@gmail.com>
2019-09-07 00:27:58 -0700

    Uploads display to page

--- client/src/pages/Profile/profile.js
+++ client/src/pages/Profile/profile.js
@@ -2,22 +2,22 @@ import React, { Component } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import  { logoutUser } from "./../../actions/authActions";
+import { logoutUser } from "./../../actions/authActions";
 import { Link } from "react-router-dom";
 import "./profile.css";
 import Navbar from "../../components/NavBar/navbar";
 import Header from "../../components/Header/header";
-import {Container} from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 // import 'semantic-ui-css/semantic.min.css';
 
 
 
 
-class Profile extends Component { 
+class Profile extends Component {
   constructor() {
     super();
     this.state = {
-
+      uploads: []
     }
   }
 
@@ -26,12 +26,14 @@ class Profile extends Component {
 
     axios.get("/api/posts/uploads/" + this.props.auth.user.id)
       .then(res => {
-        console.log(res.data);
+        this.setState({ uploads: res.data });
+        // console.log(res.data);
+        console.log(this.state.uploads);
       })
       .catch(err => console.log(err));
   }
 
-  onLogoutClick  = event => {
+  onLogoutClick = event => {
     event.preventDefault();
     // this.props.logoutUser();
 
@@ -39,31 +41,41 @@ class Profile extends Component {
     console.log("logged out");
 
   }
-  
+
   render() {
     const { user } = this.props.auth;
     console.log(user);
     return (
       <React.Fragment>
-      <Header/>
-      <Navbar/>
+        <Header />
+        <Navbar />
 
         <Container >
 
-            {/* <div>
+          {/* <div>
                 <h1>{user.name}</h1>
               </div> */}
 
-            <button
-              onClick={this.onLogoutClick}
-              >Logout</button>
-            
-            <Link to="/upload" className="btn-flat waves-effect">
-              Upload
+          <button
+            onClick={this.onLogoutClick}
+          >Logout</button>
+
+          <Link to="/upload" className="btn-flat waves-effect">
+            Upload
             </Link>
-          </Container>
-          </React.Fragment>
-        );
+          {this.state.uploads.map(function (pic, i) {
+            return (
+              <div key={pic.id} id={pic.userId} className="card" style={{margin: "25px", width: "700px", height: "100%" }}>
+                <h2 className="title" style={{ fontSize: "50px" }}>{pic.title}</h2>
+                <img src={pic.picUrl} style={{ width: "500px", height: "400px", paddingLeft: "125px" }} alt="user photo" />
+                <p className="caption" style={{ margin: "50px" }}>{pic.caption}</p>
+              </div>
+            )
+          })}
+        </Container>
+
+      </React.Fragment>
+    );
   }
 }
 
@@ -77,6 +89,6 @@ const mapStateToProps = state => ({
 });
 
 export default connect(
-    mapStateToProps,
-    { logoutUser }
+  mapStateToProps,
+  { logoutUser }
 )(Profile);
